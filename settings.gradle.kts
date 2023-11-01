d507f353d8880e7f329cfb66421368f5ec66b901
q2ve <qwq2eq@gmail.com>
2023-11-01 20:20:01 +0700

    Added initial root config. Root build.gradle etc.

---
+++ settings.gradle.kts
@@ -0,0 +1,32 @@
+import org.json.JSONArray
+
+pluginManagement {
+	repositories {
+		google()
+		mavenCentral()
+		gradlePluginPortal()
+	}
+}
+
+buildscript {
+	repositories {
+		mavenCentral()
+	}
+
+	dependencies {
+		classpath("org.json:json:20231013")
+	}
+}
+
+rootProject.name = "Personal Multiapp"
+
+// Applying modules.
+val modulesJson = JSONArray(File("modules_list.json").readText())
+modulesJson.forEach {
+	val moduleObject = it as org.json.JSONObject
+	val module = moduleObject.getString("module")
+	val path = moduleObject.getString("path")
+	include(module)
+	project(module).projectDir = File(rootDir, path)
+	println("Include module $module by path rootDir/$path")
+}
